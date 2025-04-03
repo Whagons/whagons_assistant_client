6161a3d7f1f280be8d826b2b352febd0b41d6002
desarso <malek.gabriel33@gmail.com>
2025-04-03 03:50:52 -0700

    host fix

--- solidjs/vite.config.ts
+++ solidjs/vite.config.ts
@@ -101,6 +101,7 @@ export default defineConfig({
     }
   },
   preview: {
-    host: process.env.VITE_CHAT_HOST_DEV
+    host: process.env.VITE_CHAT_HOST_DEV,
+    allowedHosts: ['nca-assistant.gabrielmalek.com', 'nca-assistant.development.gabrielmalek.com']
   }
 });
